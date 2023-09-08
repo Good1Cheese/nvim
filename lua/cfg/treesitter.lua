f4382816a0464e8d862a1bfc23c9026f21519f6c
Good1Cheese <60037254+Good1Cheese@users.noreply.github.com>
2023-09-08 12:27:57 +0300

    :wrench: Refactored treesitter.lua

--- lua/cfg/treesitter.lua
+++ lua/cfg/treesitter.lua
@@ -1,29 +1,8 @@
-local configs = require("nvim-treesitter.configs")
-
-configs.setup {
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
-    -- Add a language of your choice
-    ensure_installed = { "cpp", "python", "lua", "javascript", },
+require("nvim-treesitter.configs").setup({
+    ejnsure_installed = { "cpp", "python", "lua", "javascript" },
     sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
     highlight = {
-        enable = true,       -- false will disable the whole extension
-        disable = { "" },    -- list of language that will be disabled
-        additional_vim_regex_highlighting = true,
-    },
-    indent = {
         enable = true,
-        disable = { "yaml" }
-    },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        additional_vim_regex_highlighting = false
     }
-}
--- colors = {}, -- table of hex strings
--- termcolors = {} -- table of colour name strings
+})
