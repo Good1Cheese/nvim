dadfe1cd123b36ca06f22e4a877f59fb596b4640
Good1Cheese <2916675@gmail.com>
2023-11-12 11:22:49 +0300

    :wrench: Fixed indents highlight

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,9 +4,6 @@ return {
     ---------------------
 
 
-    -- Indents Highlight
-    require("plugins.mini-indentscope"),
-
     -- Highlight under cursor
     require("plugins.vim-illuminate"),
 
@@ -52,6 +49,62 @@ return {
         opts = { theme = "onedark" },
         event = "VeryLazy"
     },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { enabled = false },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+            },
+        },
+        main = "ibl",
+    },
+
+    {
+        "echasnovski/mini.indentscope",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        opts = {
+            -- symbol = "▏",
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm", },
+                    callback = function()
+                        vim.b.miniindentscope_disable = true
+                    end,
+                })
+            end,
+        },
+
 
     -------------------------
     ---- EDITING PLUGINS ----
