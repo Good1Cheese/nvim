c10e12e8cfc7c472f4d5b8e4d52f36ffff46d4ca
Good1Cheese <2916675@gmail.com>
2023-11-13 22:34:03 +0300

    :sparkles: Added default keymaps for LSP

--- lua/cfg/lsp/lsp.lua
+++ lua/cfg/lsp/lsp.lua
@@ -1,24 +1,10 @@
 -- LSP
 local lsp = require("lsp-zero")
-
 lsp.preset("recommended")
 
--- lsp.on_attach(function(client, bufnr)
---     local caps = client.server_capabilities
---     if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
---         local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
---         vim.api.nvim_create_autocmd("TextChanged", {
---             group = augroup,
---             buffer = bufnr,
---             callback = function()
---                 vim.lsp.buf.semantic_tokens_full()
---             end,
---         })
---         -- fire it first time on load as well
---         vim.lsp.buf.semantic_tokens_full()
---     end
---     --...
--- end)
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
 
 lsp.set_server_config({
     on_init = function(client)
