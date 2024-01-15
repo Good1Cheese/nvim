44497e3dbad7f379175a2c0009b2c41c134df787
Good1Cheese <2916675@gmail.com>
2024-01-15 09:46:48 +0300

    :wrench: Dashboard .3.1

--- lua/cfg/dashboard.lua
+++ lua/cfg/dashboard.lua
@@ -1,22 +1,36 @@
-local art = {
-    "           .'\\   /`.           ",
-    "         .'.-.`-'.-.`.         ",
-    "    ..._:   .-. .-.   :_...    ",
-    "  .'    '-.(o ) (o ).-'    `.  ",
-    " :  _    _ _`~(_)~`_ _    _  : ",
-    ":  /:   ' .-=_   _=-. `   ;\\  :",
-    ":   :|-.._  '     `  _..-|:   :",
-    " :   `:| |`:-:-.-:-:'| |:'   : ",
-    "  `.   `.| | | | | | |.'   .'  ",
-    "    `.   `-:_| | |_:-'   .'    ",
-    "      `-._   ````    _.-'      ",
-    "          ``-------''          "
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+local logo = [[
+           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
+           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
+           ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+           ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+           ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+      ]]
+
+dashboard.section.header.val = vim.split(logo, "\n")
+
+dashboard.section.buttons.val = {
+    dashboard.button("f", " " .. "Find file", ":Telescope find_files <cr>"),
+    dashboard.button("n", " " .. "New file", ":ene <BAR> startinsert <cr>"),
+    dashboard.button("r", " " .. "Recent files", ":Telescope oldfiles <cr>"),
+    dashboard.button("g", " " .. "Find text", ":Telescope live_grep <cr>"),
+    dashboard.button("c", " " .. "Config", ":e $MYVIMRC <cr>"),
+    dashboard.button("s", " " .. "Restore Session", [[:lua require("persistence").load() <cr>]]),
+    dashboard.button("l", "󰒲 " .. "Lazy", ":Lazy<cr>"),
+    dashboard.button("q", " " .. "Quit", ":qa<cr>"),
 }
 
-require("dashboard").setup({
-    header = art,
-    directories = {
-        "~/.config/nvim",
-        "~/Projects",
-    },
-})
+for _, button in ipairs(dashboard.section.buttons.val) do
+    button.opts.hl = "AlphaButtons"
+    button.opts.hl_shortcut = "AlphaShortcut"
+end
+
+dashboard.section.footer.val = function() return "Don't know what to say..." end
+dashboard.section.header.opts.hl = "AlphaHeader"
+dashboard.section.buttons.opts.hl = "AlphaButtons"
+dashboard.section.footer.opts.hl = "AlphaFooter"
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
