0f90560c5f8aa3049dc2834806aa1ad7a40257f5
Good1Cheese <2916675@gmail.com>
2024-02-13 21:19:02 +0300

    :wrench: Removed lualine

--- lua/cfg/lualine.lua
+++ lua/cfg/lualine.lua
@@ -1,29 +1,30 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
-
-require("lualine").setup {
-    options = {
-        theme = "auto",
-        disabled_filetypes = { "dashboard", "neo-tree" },
-        component_separators = "|",
-        section_separators = { left = "", right = "" },
-    },
-    sections = {
-        lualine_a = {
-            { "mode", separator = { left = "" }, right_padding = 2 },
-        },
-        lualine_b = { "filename", "branch" },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = { "filetype", "progress" },
-        lualine_z = {},
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-    },
-}
+-- -- Bubbles config for lualine
+-- -- Author: lokesh-krishna
+--
+-- require("lualine").setup {
+--     options = {
+--         section_separators = '',
+--         component_separators = '',
+--         theme = "auto",
+--         disabled_filetypes = { "dashboard", "neo-tree" },
+--         component_separators = "|",
+--     },
+--     -- sections = {
+--     --     lualine_a = {
+--     --         { "mode", --[[ separator = { left = "" }, ]] --[[ right_padding = 2 ]] },
+--     --     },
+--     --     -- lualine_b = { "filename", "branch" },
+--     --     lualine_c = {},
+--     --     lualine_x = {},
+--     --     lualine_y = { "filetype", "progress" },
+--     --     lualine_z = {},
+--     -- },
+--     -- inactive_sections = {
+--     --     lualine_a = {},
+--     --     lualine_b = {},
+--     --     lualine_c = {},
+--     --     lualine_x = {},
+--     --     lualine_y = {},
+--     --     lualine_z = {},
+--     -- },
+-- }
