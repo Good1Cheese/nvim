27ad4c1c69493dd955ddac710952a7d9398e11ce
Good1Cheese <2916675@gmail.com>
2024-02-14 22:08:30 +0300

    :wrench: Edited transparency

--- ginit.vim
+++ ginit.vim
@@ -3,7 +3,8 @@
 
 " Set Editor Font
 if exists("g:neovide")
-    let g:neovide_transparency = 0.9
+    " let g:neovide_transparency = 0.8
+    " let g:neovide_window_blurred = true
     let g:neovide_refresh_rate = 90
     set guifont=JetBrainsMono\ Nerd\ Font:h12
 endif
--- lua/cfg/toggleterm.lua
+++ lua/cfg/toggleterm.lua
@@ -1,7 +1,7 @@
-function _G.set_terminal_keymaps()
-    local opts = { buffer = 0 }
-    vim.keymap.set("t", "<esc>", "<cmd>q<cr>", opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+-- function _G.set_terminal_keymaps()
+--     local opts = { buffer = 0 }
+--     vim.keymap.set("t", "<esc>", "<cmd>q<cr>", opts)
+-- end
+--
+-- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+-- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
