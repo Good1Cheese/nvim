a07cc1a326ef99d911898f21f0a511e4eb706eb2
Good1Cheese <work.cheese@erine.email>
2024-08-20 14:09:23 +0900

    coderunnner edit

--- lua/plugins/coderunner.lua
+++ lua/plugins/coderunner.lua
@@ -3,7 +3,26 @@ local Plugin = { "CRAG666/code_runner.nvim" }
 Plugin.cmd = "RunFile"
 
 Plugin.opts = {
+	mode = "float",
 	focus = false,
+	float = {
+		close_key = "<ESC>",
+		-- Window border (see ':h nvim_open_win')
+		border = "single",
+
+		-- Num from `0 - 1` for measurements
+		height = 1,
+		width = 0.8,
+		x = 0.5,
+		y = 0.5,
+
+		-- Highlight group for floating window/border (see ':h winhl')
+		border_hl = "FloatBorder",
+		float_hl = "Normal",
+
+		-- Transparency (see ':h winblend')
+		blend = 0,
+	},
 	filetype = {
 		cpp = {
 			"cd $dir &&",
@@ -18,6 +37,16 @@ Plugin.opts = {
 					"cd $dir &&",
 					"gcc $fileName",
 					"-o $fileNameWithoutExt",
+				}
+			end
+
+			if string.find(file, "tests") then
+				return {
+					"cd $dir &&",
+					"gcc -std=c18 -Wall -Wextra -lcriterion",
+					"$fileName solution.c",
+					"-o /tmp/$fileNameWithoutExt &&",
+					"/tmp/$fileNameWithoutExt",
 				}
 			end
 
