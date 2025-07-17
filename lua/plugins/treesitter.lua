7ccc3f6785788b6aaa236c2c0d81d5c68f330883
Good1Cheese <map.omebauw@erine.email>
2025-07-17 16:07:09 +0900

    upt

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ local Plugin = { "nvim-treesitter/nvim-treesitter" }
 Plugin.event = { "BufReadPre", "BufNewFile" }
 Plugin.branch = "main"
 
+-- TSInstall lua c_sharp bash java xml csv json c cpp python dockerfile
+
 function Plugin.config()
 	require("nvim-treesitter").setup {
 		-- Directory to install parsers and queries to
