d75676273a202be9fd085b3bcc1e086e9b074283
Good1Cheese <map.omebauw@erine.email>
2025-07-28 20:09:16 +0900

    auto get lsp servers list

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,16 +1,19 @@
 local M = {}
 
--- List of enabled LSP servers
-M.servers = {
-	"lua_ls",
-	"gopls",
-	"bashls",
-	"pylsp",
-	"docker_compose_language_service",
-	"dockerls",
-	"omnisharp",
-	"jdtls"
-}
+-- Automatically get list of servers from files in servers/ directory
+M.get_servers = function()
+	local servers_dir = vim.fn.stdpath("config") .. "/lua/lsp/servers"
+	local files = vim.fn.glob(servers_dir .. "/*.lua", false, true)
+	local servers = {}
+
+	for _, file in ipairs(files) do
+		-- Extract server name from filename (remove path and .lua extension)
+		local server = file:match("([^/]+)%.lua$")
+		table.insert(servers, server)
+	end
+
+	return servers
+end
 
 -- Setup function
 M.setup = function()
@@ -21,8 +24,9 @@ M.setup = function()
 		root_markers = { ".git" },
 	})
 
-	for _, server in ipairs(M.servers) do
-		-- config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+	-- Get servers automatically from directory
+	local servers = M.get_servers()
+	for _, server in ipairs(servers) do
 		vim.lsp.config(server, require("lsp.servers." .. server))
 		vim.lsp.enable(server)
 	end
