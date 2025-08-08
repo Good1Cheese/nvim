0dd7ff8c20443365d06f16f3ea94be66793f54b6
Good1Cheese <map.omebauw@erine.email>
2025-08-08 10:39:38 +0900

    upt

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "fredrikaverpil/neotest-golang",
+        "rouge8/neotest-rust",
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "antoinemadec/FixCursorHold.nvim",
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-golang"),
+                require("neotest-rust"),
+            },
+        })
+
+
+        -- ============================================================================
+        -- NEOTEST
+        -- ============================================================================
+        vim.keymap.set("n", "<leader>Tt", function() require("neotest").run.run() end, { desc = "Run nearest test" })
+        vim.keymap.set("n", "<leader>Tf", function() require("neotest").run.run(vim.fn.expand("%:p")) end,
+            { desc = "Run all tests in file" })
+        vim.keymap.set("n", "<leader>Ts", function() require("neotest").summary.toggle() end,
+            { desc = "Toggle test summary" })
+    end,
+}
